8f7305c82ecf6b5373a02aa575a47a9e086f0cc5
delTh <delth@cent.lan>
2016-03-11 23:15:30 +0300

    урок 2.3

--- web/app/ask/qa/models.py
+++ web/app/ask/qa/models.py
@@ -1,5 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models.User import User
 
 # Create your models here.
+
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DataTimeField(blank=true)
+	rating = models.IntegerField(11)
+	author = ForegingKey(User)
+	likes = models.ManyToManyField(User)
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DataTimeField(blank=true)
+	question = ForegingKey(Question)
+	author = ForegingKey(User)

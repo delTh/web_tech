72d599f8e5de316d34fba41bfcf9d819bc17dd28
delTh <k.519@mail.ru>
2016-03-17 16:37:52 +0000

    пройден 1-тест

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -10,12 +10,12 @@ class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
 	added_at = models.DateTimeField(auto_now_add=True)
-	rating = models.IntegerField(11)
-	author = models.OneToOneField(User)
+	rating = models.IntegerField(11, default=0)
+	author = models.ForeignKey(User)
 	likes = models.CharField(max_length=255)
 
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField(auto_now_add=True)
 	question = models.ForeignKey(Question)
-	author = models.OneToOneField(User)
+	author = models.ForeignKey(User)
